f7cd069a3b8339b2152be917a277eb02b55436b0
chastity duhaney <chastity_duhaney82@gmail.com>
2019-04-06 20:57:06 -0400

    menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,13 +16,14 @@ function toggleMenu() {
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
+    //Set Menu State
+    showMenu = true;
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.add("show"));
-    //Set Menu State
     showMenu = false;
   }
 }
