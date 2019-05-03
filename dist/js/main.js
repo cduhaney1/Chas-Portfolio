8d1dada3dec54b9d0b9d5da52dde166f485f4bff
chastity duhaney <chastity_duhaney82@gmail.com>
2019-05-03 03:14:14 -0400

    fixed styling on about me page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,10 @@
 
 // select DOM Items
-const menuBtn = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu");
-const menuNav = document.querySelector(".menu-nav");
-const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll("nav-item");
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
 
 //Set Initial State of Menu
 let showMenu = false;
@@ -24,7 +24,7 @@ function toggleMenu() {
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
-    navItems.forEach(item => item.classList.add("show"));
+    navItems.forEach(item => item.classList.remove("show"));
     showMenu = false;
   }
 }
